300d8b8e1648c079c4d69f3ad17bef0dbbdb1524
MarkEhab33 <markoehab33@gmail.com>
2022-12-17 00:26:30 +0200

    splash screen & Bottom navigation bar

---
+++ lib/Home/task_list/AddTaskBottomSheet.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddTaskBotoomSheet extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/MyTheme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyTheme{
+static Color primaryLightColor = Color(0xFF5D9CEC);
+static Color whiteColor = Colors.white;
+static Color greenColor = Color(0xFF61E757);
+static Color redColor = Color(0xFFEC4B4B);
+static Color blackColor = Color(0xFF363636);
+static Color backGroundColorLight = Color(0xFFDFECDB);
+static Color greyColor = Color(0xFFC8C9CB);
+
+static Color primaryDarkColor  = Color(0xFF060E1E);
+
+static ThemeData lightTheme=ThemeData(
+  primaryColor: primaryLightColor,
+  appBarTheme: AppBarTheme(backgroundColor: primaryLightColor,
+    elevation: 0,
+
+  ),
+  primaryTextTheme: TextTheme(
+    headline1: TextStyle(
+      fontSize: 22,
+      fontWeight: FontWeight.bold,
+      color: whiteColor,
+    )
+
+  ),
+scaffoldBackgroundColor: backGroundColorLight,
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: primaryLightColor,
+     backgroundColor: Colors.transparent,
+    elevation: 0
+  )
+);
+
+
+}
