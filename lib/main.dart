a28ae08ba5dade58929630ba04da12c13e56c98f
MarkEhab33 <markoehab33@gmail.com>
2023-01-26 05:39:10 +0200

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_app/Home/HomeScreen.dart';
 import 'package:todo_app/MyTheme.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseFirestore.instance.disableNetwork();
+
   runApp(const MyApp());
 }
 
